0b9eb7e329acd9d496e4c2af5c8d423b78a332f9
bill <>
2017-11-24 20:47:22 +0000

    alphanumeric only in token names

--- laundry.py
+++ laundry.py
@@ -6,28 +6,36 @@ import threading
 import time
 from ftplib import FTP
 from cStringIO import StringIO
+import cred
 
+POLLSECONDS=10
+
+# Python 2.7 because thats on my Raspberry Pi
 # this is the embedded client
 # intended for auto execution on raspberry pi boot
 # run in directory that has the unused tokens
+# manually create a UsedTokens subdir
+# non git cred.py needed for SERVER,USER,PASSWORD
 # will move tokens to UsedTokens subdirectory when a token is entered by user via keyboard
 # tokens are files with names ending in .tok that have 6 chars in filename, eg abcdef.tok
 # uses a thread for keyboard input of tokens (first char selects machine, following six are token)
 # if the token is used, a relay is turned on and off for 1 second
-# the relay can energize a washer or dryer
+# the relay can energize a washer or dryer by just replacing the switch in the coon slider mechanism with circuit wired to relay
 # the server, upon payment, will create token files that are replicated to the client via simple ftp commands
 # replication is a simple polling process every 10 minutes (about 30MB per month, which can be had for about $10/month)
-# if the token file exists on server but not client, it is created in the client (directory above UsedTokens)
-# if the token file has been moved to UsedTokens, actions are taken to inform the server of the token use.
+# when I tried push using Goodsync mediator, the data usage to keep connection alive was nearly 2GB per month. Polling can tear down connection after just a few milliseconds and this uses only 30MB per month.
+# if the token file exists on server but not client, it is created in the client (directory above UsedTokens), thus available to a tenant to type in and activate laundry machine
+# if the token file has been moved to UsedTokens, actions are repeatedly taken to inform the server of the token use until successful
 
 #todo:
 # no revocation yet 
 # limit polling hours to 6am to 11pm
 # merge with relay usb board commands
 
+startupdir=''
 quitnow=0
 
-with open("TestFtp.py") as fp:
+with open("laundry.py") as fp:
     for i, line in enumerate(fp):
         if "\xe2" in line:
             print i, repr(line)
@@ -43,8 +51,10 @@ def readserver(ftp):
             server_tokens.append( line.split(";")[4][1:]) 
     ftp.cwd("UsedTokens")
     tokens=[]
-    ftp.retrlines('MLSD', tokens.append)
-    ftp.cwd('..')
+    try:
+        ftp.retrlines('MLSD', tokens.append)
+    finally:
+        ftp.cwd('..')
     server_used_tokens=[]
     for line in tokens:
         if (line.split(";")[0]=="Type=file"):
@@ -70,16 +80,18 @@ def readlocal():
     
 def keyboard_loop():
     global quitnow
+    global startupdir
     print 'keyboard loop started ...'
     a = ''
     while (a != 'quit'):
         a=raw_input('enter the 2abcdef')
+        a=filter(str.isalnum, a)
+        print a
         if len(a)==7:
             m=a[0:1]
             t=a[1:7].lower()+'.tok'
-            print t
             try:
-                os.rename(t,'UsedTokens/'+t)
+                os.rename(startupdir+'/'+t,startupdir+'/UsedTokens/'+t)
                 print 'switched'
             except:
                 print 'not switched'
@@ -96,7 +108,10 @@ def transfertokens(ftp, st,su,lt,lu):
             f.close()
     for l in lu:
         print 'delete ' + l + ' in server tokens'
-        ftp.delete(l)
+        try:
+            ftp.delete(l)
+        except:
+            pass
         print 'create ' + l + ' in server used'
         f=open('UsedTokens/'+l,'r') # could be any small file
         try:
@@ -108,19 +123,21 @@ def transfertokens(ftp, st,su,lt,lu):
         
 t=threading.Thread(target=keyboard_loop)
 t.start()
-
+startupdir=os.getcwd()
+print startupdir
 while quitnow==0:
     try:
-        ftp = FTP("xxxx")
+        ftp = FTP(cred.SERVER)
         try:
-            ftp.login("brooksidelaundry", "yxxx")
+            ftp.login(cred.USER, cred.PASSWORD)
             st,su=readserver(ftp)
             lt,lu=readlocal()
             transfertokens(ftp,st,su,lt,lu)
         except:
             print 'ExceptioN caught ', sys.exc_info()
-        ftp.quit()
+        finally:
+            ftp.quit()
     except:
         print 'Exception caught ', sys.exc_info()
-    time.sleep(60)
+    time.sleep(POLLSECONDS)
     
